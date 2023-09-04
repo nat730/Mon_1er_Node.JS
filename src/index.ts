1b154df0201fbbd623dfbd535f977f8970f365e9
nat730 <natanaelcolart@gmail.com>
2023-09-04 15:14:31 +0200

    2023-09-04 - 1er exo fini

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,17 @@
-console.log('Hello world');
+if (process.argv.includes("--help")) {
+  console.log("Je vais vous aider");
+}
+
+
+
+
+const Name = process.argv.indexOf("--name");
+if (Name == -1) {
+  console.log("Veuillez spécifier un nom après 'start'.");
+}
+
+else {
+  const name = process.argv[Name+1];
+  console.log(`Bonjour ${name}`);
+} 
+
